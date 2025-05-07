9c7c9d1c52024e64c7ce01cc718743537c426ea4
vinhngph <136446610+vinhngph@users.noreply.github.com>
2025-05-07 18:00:38 +0700

    feat: add GET, POST, PUT APIs for /document

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
+require("dotenv").config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const documentRouter = require("./routes/documentRoutes")
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/document', documentRouter)
 
 module.exports = app;
